25238019ab56915c4c45e4253d7dc5126aeab542
oscartom <81743938+OscarTom@users.noreply.github.com>
2023-03-22 10:51:34 +0100

    Modificando None en clase User_08

--- db/models/user.py
+++ db/models/user.py
@@ -1,8 +1,10 @@
 ### MODEL USER DB API ###
 from pydantic import BaseModel
+from typing import Optional
 
 #Declaramos la entidad user
 class User(BaseModel):
-  id:str   # con esto "| None" indicamos que puede ser opcional
+  #id:str | None   # con esto "| None" indicamos que puede ser opcional
+  id: Optional[str]
   username:str
   email:str
